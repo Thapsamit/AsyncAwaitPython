4788ee93bdedc5e0c96aec5fdcaf2c143a1766e9
Amit Thapliyal <67275253+Thapsamit@users.noreply.github.com>
2022-10-05 13:53:54 +0000

    sleep

--- main.py
+++ main.py
@@ -9,4 +9,19 @@ async def main():
 asyncio.run(main())  # creating event loop
 
 
+async def foo():
+  print("Amit")
+  task = asyncio.create_task(goo("Execute after 3 seconds"))
 
+  print(
+    "finished after waiting 3 seconds"
+  )  # instead create a task to execute this line means allow this lie to execute as previous LOC is taking time
+
+
+async def goo(text):
+
+  await asyncio.sleep(3)
+  print(text)
+
+
+asyncio.run(foo())
